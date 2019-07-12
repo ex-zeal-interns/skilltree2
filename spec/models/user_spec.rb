fbb9144f26858a7983408802c87c5d0f2c014889
CodeCopyCoffee <gdverrastro@gmail.com>
2019-07-11 17:16:23 -0700

    Removed views and controllers we didn't change, used a partial for the form, added tests for each validation, deleted lines that were commented out, did a db rollback, and did a new migration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not sign up with existing email" do
+    user = User.new(email: "test@email.com", first_name: "Bob", last_name: "Smith", time_zone: "PT")
+    user.save
+    user2 = User.new(email: "test@email.com", first_name: "Sally", last_name: "Jones", time_zone: "ET")
+    expect(user2).to_not be_valid
+  end
+
+  it "should not sign up with missing email" do
+    user = User.new(first_name: "Bob", last_name: "Smith", time_zone: "PT")
+    expect(user).to_not be_valid
+  end
+
+  it "should not sign up with missing first name" do
+    user = User.new(email: "test@email.com", last_name: "Smith", time_zone: "PT")
+    expect(user).to_not be_valid
+  end
+
+  it "should not sign up with missing last name" do
+    user = User.new(email: "test@email.com", first_name: "Bob", time_zone: "PT")
+    expect(user).to_not be_valid
+  end
+
+  it "should not sign up with missing time zone" do
+    user = User.new(email: "test@email.com", first_name: "Bob", last_name: "Smith")
+    expect(user).to_not be_valid
+  end
 end
