d6055ccda219b4673df84b2c58d58fe1edaacd5b
CodeCopyCoffee <gdverrastro@gmail.com>
2019-07-12 09:53:59 -0700

    Removed commented functionality

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
   # POST /resource
   def create
@@ -12,17 +8,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super do |resource|
     end
   end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
