801e45836d6289a07439442df93c8868781e1158
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-15 14:15:01 -0700

    prettier pre-commit test

--- app/javascript/components/Routes.js
+++ app/javascript/components/Routes.js
@@ -1,41 +1,45 @@
-import React from "react"
-import ReactDOM from 'react-dom'
-import PropTypes from "prop-types"
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import StaticProfile from './StaticProfile'
-import Profile from './Profile'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import StaticProfile from "./StaticProfile";
+import Profile from "./Profile";
 
 class Routes extends React.Component {
-  constructor(props){
-    super(props)
-      this.state = {
-        profile: {
-          firstname: 'John',
-          lastname: 'Doe',
-          email: 'john@example.com',
-          timezone: 'UTC',
-          url: 'iou22o38f4',
-        },
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john@example.com",
+        timezone: "UTC",
+        url: "iou22o38f4"
       }
-    }
+    };
+  }
 
-  render () {
-      const {profile} = this.state
+  render() {
+    const { profile } = this.state;
 
-      // builds custom url
-      const profUrl = `/profile/${profile.url}`
+    // builds custom url
+    const profUrl = `/profile/${profile.url}`;
 
     return (
       <Router>
-        <Route path="/profile" exact render={(props) => <Profile{...props}
-          profile={profile}
-        />} />
-        <Route path={profUrl} exact render={(props) => <StaticProfile{...props}
-          profile={profile}
-        />} />
+        <Route
+          path="/profile"
+          exact
+          render={props => <Profile {...props} profile={profile} />}
+        />
+        <Route
+          path={profUrl}
+          exact
+          render={props => <StaticProfile {...props} profile={profile} />}
+        />
       </Router>
     );
   }
 }
 
-export default Routes
+export default Routes;
