ccc752f8fc56cabb02e1b32d0d8bdbec3856c3a3
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-16 13:49:46 -0700

    cleaning components to resolve deployment issue with enzyme

--- app/javascript/components/Routes.js
+++ app/javascript/components/Routes.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import StaticProfile from "./StaticProfile";
 import Profile from "./Profile";
--- app/javascript/components/StaticProfile.js
+++ app/javascript/components/StaticProfile.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import ReactDOM from "react-dom";
 
 class StaticProfile extends React.Component {
   render() {
