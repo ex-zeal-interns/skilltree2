24bf3b6e20533819300d83c14d5023d53ed6bef2
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-16 13:52:37 -0700

    added test files to __tests__

---
+++ app/javascript/components/__tests__/App.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
+import Routes from "./Routes";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<App>", () => {
+  const wrapper = shallow(<App />);
+  it("renders without crashing", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("has a link to the dashboard (<Profile/>)", () => {
+    expect(wrapper.find('[href="/profile"]')).toHaveLength(1);
+  });
+  it("has a router", () => {
+    expect(wrapper.find(Routes)).toHaveLength(1);
+  });
+});
---
+++ app/javascript/components/__tests__/StaticProfile.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import StaticProfile from "./StaticProfile";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<StaticProfile>", () => {
+  const profile = {
+    firstname: "John",
+    lastname: "Doe",
+    email: "john@example.com",
+    timezone: "UTC",
+    url: "iou22o38f4"
+  };
+  const wrapper = shallow(<StaticProfile profile={profile} />);
+  it("renders corretly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("displays user fullname", () => {
+    expect(wrapper.find("#fullname").length).toEqual(1);
+  });
+  it("displays user email", () => {
+    expect(wrapper.find("#email").length).toEqual(1);
+  });
+  it("displays user timezone", () => {
+    expect(wrapper.find("#timezone").length).toEqual(1);
+  });
+});
