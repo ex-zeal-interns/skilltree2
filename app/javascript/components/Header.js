778e8f540c9473afcbb5f85ac4707a09212ce05f
CodeCopyCoffee <gdverrastro@gmail.com>
2019-07-19 13:10:06 -0700

    Re-committing and re-attempting to push tests and Header component

---
+++ app/javascript/components/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Logo from "./logo.png";
+
+class Header extends Component {
+  render() {
+    const {
+      logged_in,
+      sign_out,
+      sign_in,
+      current_user,
+      edit_user
+    } = this.props;
+    return (
+      <div className="white-header">
+        <img id="zeal-img" className="navLogo" src={Logo} />
+        <div className="navLinks">
+          <a
+            className="navBtn"
+            id="profileBtn"
+            href={(logged_in && `/profile/${current_user.id}`) || sign_in}
+          >
+            My Dashboard
+          </a>
+          <a
+            className="navBtn"
+            id="signInOut"
+            href={(logged_in && sign_out) || sign_in}
+          >
+            {(logged_in && "Log Out") || "Log In"}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
